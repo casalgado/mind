26bfb43db8e3a1b22d3999ed99d968c56a07f224
casalgado <casalgado86@yahoo.com>
2018-02-21 18:20:54 -0500

    first commit

---
+++ public/js/layouts/load_global_styles.js
@@ -0,0 +1,6 @@
+document.write('\
+\
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">\
+	<link rel="stylesheet" type="text/css" href="css/layouts/navigation.css">\
+	<link rel="stylesheet" type="text/css" href="css/layouts/global.css">\
+');
---
+++ public/js/layouts/load_navigation.js
@@ -0,0 +1,10 @@
+document.write('\
+\
+	<div class="navigation">\
+		<ul class="navigation-links">\
+			<li><a href="expansion.html">expansion</a></li>\
+			<li><a href="www.google.com">contact</a></li>\
+			<li><a href="contact.html">contact</a></li>\
+		</ul>\
+	</div>\
+');
