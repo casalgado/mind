d94bfc9598528bad4ed09371b2e1129cd115c1e0
casalgado <casalgado86@yahoo.com>
2018-02-25 09:13:30 -0500

    breadth

--- js/layouts/load_global_styles.js
+++ js/layouts/load_global_styles.js
@@ -1,5 +1,12 @@
+// loads the following stylesheets:
+// bootstrap css
+// open sans google font
+// css for navigation bar
+// global css file
+
 document.write('\
 \
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/navigation.css">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/global.css">\
--- js/layouts/load_navigation.js
+++ js/layouts/load_navigation.js
@@ -2,7 +2,7 @@ document.write('\
 \
 	<div class="navigation">\
 		<ul class="navigation-links">\
-			<li><a href="expansion.html">expansion</a></li>\
+			<li><a href="breadth.html">breadth</a></li>\
 			<li><a href="www.google.com">contact</a></li>\
 			<li><a href="contact.html">contact</a></li>\
 		</ul>\
