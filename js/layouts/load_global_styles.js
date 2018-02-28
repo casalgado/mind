c564ce6cf692431a187095bc47442f3324209a24
casalgado <casalgado86@yahoo.com>
2018-02-28 12:23:34 -0500

    breadth: mouse cursos values now responsive

--- js/layouts/load_global_styles.js
+++ js/layouts/load_global_styles.js
@@ -1,6 +1,6 @@
 // loads the following stylesheets:
 // bootstrap css
-// open sans google font
+// pt sans + serif google fonts
 // inconsolata google font
 // css for navigation bar
 // global css file
@@ -8,7 +8,7 @@
 document.write('\
 \
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">\
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i|PT+Serif:400,400i" rel="stylesheet">\
 	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/navigation.css">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/global.css">\
