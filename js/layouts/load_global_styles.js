2fab0e5ead2b019016cfcbc964a557c68ff72f89
casalgado <casalgado86@yahoo.com>
2018-02-28 11:04:52 -0500

    navbar font to inconsolata

--- js/layouts/load_global_styles.js
+++ js/layouts/load_global_styles.js
@@ -1,6 +1,7 @@
 // loads the following stylesheets:
 // bootstrap css
 // open sans google font
+// inconsolata google font
 // css for navigation bar
 // global css file
 
@@ -8,6 +9,7 @@ document.write('\
 \
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">\
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/navigation.css">\
 	<link rel="stylesheet" type="text/css" href="css/layouts/global.css">\
 ');
--- js/layouts/load_navigation.js
+++ js/layouts/load_navigation.js
@@ -3,8 +3,6 @@ document.write('\
 	<div class="navigation">\
 		<ul class="navigation-links">\
 			<li><a href="breadth.html">breadth</a></li>\
-			<li><a href="www.google.com">contact</a></li>\
-			<li><a href="contact.html">contact</a></li>\
 		</ul>\
 	</div>\
 ');
